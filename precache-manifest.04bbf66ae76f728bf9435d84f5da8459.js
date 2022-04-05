1bf56933bff6474bff747e1b996ec1453817d3d1
shayan013 <shayanakhtar013@gmail.com>
2022-04-05 18:22:43 +0500

    Updates

---
+++ precache-manifest.04bbf66ae76f728bf9435d84f5da8459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a96b96326693f5abd8ce88cf79b5970",
+    "url": "/photoGallery/index.html"
+  },
+  {
+    "revision": "37ec656ca3ce9d7c56c6",
+    "url": "/photoGallery/static/css/main.bbbd07f5.chunk.css"
+  },
+  {
+    "revision": "baa2b9b72b48c833e23c",
+    "url": "/photoGallery/static/js/2.cace878d.chunk.js"
+  },
+  {
+    "revision": "072c7c45afc2dc3f328c5fd953fe8bd2",
+    "url": "/photoGallery/static/js/2.cace878d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37ec656ca3ce9d7c56c6",
+    "url": "/photoGallery/static/js/main.976a985d.chunk.js"
+  },
+  {
+    "revision": "8500b69fbd11c5ff1534",
+    "url": "/photoGallery/static/js/runtime-main.fe683e7c.js"
+  }
+]);
